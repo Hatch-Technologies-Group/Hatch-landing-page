83965aa0f6a3e8ca497646f77bfe1544b8f76997
AcheronX. <richardhuynh53@gmail.com>
2025-12-20 18:44:34 -0800

    chore: update dependencies in package-lock.json and refine tailwind.config.js content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js,jsx,ts,tsx,vue,svelte}"
+    "./*.{html,js,jsx,ts,tsx,vue,svelte}",
+    "./!(node_modules)/**/*.{html,js,jsx,ts,tsx,vue,svelte}"
   ],
   darkMode: 'class',
   theme: {
